a483beca56b2286ad3c4cbbbeb73b6b2adf3d249
eoussama <oussama.essamadi@gmail.com>
2024-12-10 20:12:17 +0100

    python code

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def addition(a, b):
+  return a + b
+
+def multiplication(a, b):
+  return a * b
+
+if __name__ == "__main__":
+  x, y = 5, 10
+
+  print("Addition :", addition(x, y))
+  print("Multiplication :", multiplication(x, y))
